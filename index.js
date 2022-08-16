9ce1ce529730168bdca4f13312be057b92c98109
Joshua <rakichwande@gmail.com>
2022-08-16 23:11:34 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,50 @@
+function add (a,b) {
+    console.log(a+b)
+} 
+
+function subtract (a,b) {
+    console.log(a-b)
+}
+
+function multiply (a,b) {
+    console.log(a*b)
+}
+
+function divide (a,b) {
+    console.log(a/b)
+}
+
+function add(a,b) {
+    return a+b
+}
+
+function subtract(a,b) {
+    return a-b
+}
+
+function multiply(a,b) {
+    return a*b
+}
+
+function divide(a,b) {
+    return a/b
+}
+function increment(n) {
+    return n+1;
+} 
+
+function decrement(n) {
+    return n-1;
+}
+
+function makeInt(n) {
+    let a= parseInt(n,10)
+    return a;
+}
+
+function preserveDecimal(n) {
+    let a= parseFloat(n)
+    return a;
+}
+
+
